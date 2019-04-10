25067ab3e741607cd38dcbf833490c74949da2ba
C5284842 <C5284842@192.168.242.4>
2019-04-10 15:26:41 +0530

    By shiva-- Modified the Creat title Script

--- Scripts/Titles/TC_CreateTitle/Script1553008268127.groovy
+++ Scripts/Titles/TC_CreateTitle/Script1553008268127.groovy
@@ -1,4 +1,4 @@
-// Created by: Shiva
+ // Created by: Shiva
 // Description: Creating a Title
 // Created date: 3/18/2019
 // Modified date: 3/26/2019
@@ -24,6 +24,8 @@ import internal.GlobalVariable as GlobalVariable
 WebUI.callTestCase(findTestCase('Titles/TC_NavigationToTitles'), [('TitleName') : 'Title_9', ('ORG') : 'Organization', ('Titletxt') : 'Titles'], 
     FailureHandling.STOP_ON_FAILURE)
 
+Robot rob = new Robot()
+
 boolean titleAddIcon = WebUI.verifyElementVisible(findTestObject('Titles Objects/Create Title/Add_Icon'))
 
 // To write the messages into the report
@@ -32,19 +34,24 @@ KeywordLogger log = new KeywordLogger()
 if (titleAddIcon == true) {
     'Click on Add icon available on Title Summary bar'
     WebUI.click(findTestObject('Titles Objects/Create Title/Add_Icon'))
-
+	
+    
     WebUI.click(findTestObject('Titles Objects/Create Title/Effective_Start_Date'))
 
     WebUI.waitForElementVisible(findTestObject('Titles Objects/Create Title/StartDate_Period_Search'), 5)
-
+	
+	WebUI.waitForElementClickable(findTestObject('Titles Objects/Create Title/StartDate_Period_Search'), 10)
+    WebUI.click(findTestObject('Titles Objects/Create Title/StartDate_Period_Search'))
     'Select the effective start and end date'
     WebUI.setText(findTestObject('Titles Objects/Create Title/StartDate_Period_Search'), startDate)
-
-    Robot rob = new Robot()
+	Thread.sleep(2000)
+	
+	
+  
 
     rob.keyPress(KeyEvent.VK_ENTER)
 
-    Thread.sleep(2000)
+   
 
     rob.keyRelease(KeyEvent.VK_ENTER)
 
@@ -61,19 +68,24 @@ if (titleAddIcon == true) {
     //	Tname=DynamicTitleName.replace('','_') 
     //	
     //	println(Tname)
-	
     mydate = new Date()
 
+  String dynamicTitleName = CustomKeywords.'commonUtilities.TimeStamp.timeStamp'(titleName)
+
     //required format of the current date
-    currentdate_ = mydate.format('MMddYY')
-
-    currentTime_ = mydate.format('HHmmss')
-
-    timestamp = ((currentdate_ + '_') + currentTime_)
-
-    dynamicTitleName = (titleName + timestamp)
-
+    //    currentdate_ = mydate.format('MMddYY')
+    //
+    //    currentTime_ = mydate.format('HHmmss')
+    //
+    //    timestamp = ((currentdate_ + '_') + currentTime_)
+    //
+    //    dynamicTitleName = (titleName + timestamp)
+  
+  
     ' Fill the detail and Click on Save icon'
+	WebUI.waitForElementClickable(findTestObject('Titles Objects/Create Title/Name_Field'), 5)
+	
+	WebUI.click(findTestObject('Titles Objects/Create Title/Name_Field'))
     WebUI.setText(findTestObject('Titles Objects/Create Title/Name_Field'), dynamicTitleName)
 
     WebUI.waitForElementClickable(findTestObject('Titles Objects/Create Title/Name_Field'), 5)
