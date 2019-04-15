7d4642b8f10b470c508f54e533ccb8ff1d89a891
C5284612 <C5284612@192.168.237.202>
2019-04-15 14:12:34 +0530

    Added Scripts for Reason Codes

---
+++ Scripts/Reason Codes/TC_GeneralSearchReasonCodes/Script1555317701153.groovy
@@ -0,0 +1,40 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.logging.KeywordLogger as KeywordLogger
+
+WebUI.callTestCase(findTestCase('Reason Codes/TC_NavigateReasonCodes'), [:], FailureHandling.STOP_ON_FAILURE)
+
+KeywordLogger log = new KeywordLogger()
+
+String HeadingEarningCodes_Expected = WebUI.getText(findTestObject('ReasonCodesObjects/Headings_ReasonCodes'))
+
+String HeadingEarningCodes_Actual = 'Reason Codes'
+
+if (HeadingEarningCodes_Expected.equalsIgnoreCase(HeadingEarningCodes_Actual)) {
+    WebUI.click(findTestObject('ReasonCodesObjects/General Search Icon'))
+
+    WebUI.click(findTestObject('ReasonCodesObjects/General Search_Textbox'))
+
+    WebUI.setText(findTestObject('ReasonCodesObjects/General Search_Textbox'), 'TF_ReasonCodes1026')
+
+    WebUI.click(findTestObject('ReasonCodesObjects/General Search Icon'))
+
+    CustomKeywords.'globalkeywords.record.RecordNotFound'()
+
+    log.logPassed('Headings matched....Reason Codes ID is visible')
+} else {
+    log.logFailed('Headings is not matching...test case failed')
+}
+
---
+++ Scripts/Reason Codes/TC_NavigateReasonCodes/Script1555317701653.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.logging.KeywordLogger as KeywordLogger
+
+WebUI.callTestCase(findTestCase('Login_Navigation_Logout/TC_CommissionLoginPage'), [('vHeader_Name') : 'Sales Performance Home '
+        , ('vManager_Org') : 'Manage Organization'], FailureHandling.STOP_ON_FAILURE)
+
+KeywordLogger log = new KeywordLogger()
+
+String HeadingEarningCodes_Expected = WebUI.getText(findTestObject('ReasonCodesObjects/Panel_Manage Setup'))
+
+String HeadingEarningCodes_Actual = 'Manage Setup'
+
+if (HeadingEarningCodes_Expected.equalsIgnoreCase(HeadingEarningCodes_Actual)) {
+    WebUI.click(findTestObject('ReasonCodesObjects/Global Values_Link'))
+
+    WebUI.click(findTestObject('ReasonCodesObjects/ReasonCodes_Link'))
+
+    log.logPassed('Headings matched....Clicked on Reason Codes')
+} else {
+    log.logFailed('Headings is not matching...test case failed')
+}
+
